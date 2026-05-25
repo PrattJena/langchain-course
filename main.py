d637956cbb4f96ed05f5184fa7bf79c2ea8b9695
Pratyush Jena <prattjena69@gmail.com>
2026-05-24 17:58:46 -0700

    Integrating tavily client for job search

--- main.py
+++ main.py
@@ -1,34 +1,46 @@
 from dotenv import load_dotenv
-from langchain_google_genai import ChatGoogleGenerativeAI
 from langchain.agents import create_agent
 from langchain.tools import tool
 from langchain_core.messages import HumanMessage
+from langchain_google_genai import ChatGoogleGenerativeAI
+from tavily import TavilyClient
 
 load_dotenv()
+
+tavily = TavilyClient()
+
 
 @tool
 def search(query: str) -> str:
     """
     Tool that searches for information on the web.
-    
+
     Args:
-        query: The search query
-        
+        query: Clean keyword search terms. DO NOT use search operators like 'site:'.
+
     Returns:
         Search results
     """
     print(f"Searching for: {query}")
-    return f"Tokyo weather is sunny"
+    return tavily.search(query=query)
+
 
 llm = ChatGoogleGenerativeAI(model="gemini-3.5-flash")
 tools = [search]
 agent = create_agent(model=llm, tools=tools)
 
 
-
 def main():
     print("Hello from langchain-course!")
-    result = agent.invoke({"messages": [HumanMessage(content="What is the weather in Tokyo?")]})
+    result = agent.invoke(
+        {
+            "messages": [
+                HumanMessage(
+                    content="search for only 1 job posting for an ai engineer using langchain in the bay area on linkedin and list their details?"
+                )
+            ]
+        }
+    )
     print(result)
 
 
